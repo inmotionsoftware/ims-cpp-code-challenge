073cd3e1cbe4832ba8599d91b5345cff4b4203cb
Katie Antlitz <katinthecode@gmail.com>
2022-02-20 18:20:37 -0600

    Adding code to separate first string and then pairs into vector of pairs

---
+++ ims-cpp-code-challenge/ims-cpp-code-challenge/ims-cpp-code-challenge.cpp
@@ -0,0 +1,45 @@
+// ims-cpp-code-challenge.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    string start =  "TAFT";
+    //string end;
+
+    vector<pair<string, string>> pairs;
+    short loop = 0;
+    string input;
+    string delimiter = " -> ";
+    string token;
+    ifstream myFile("input.txt");
+    size_t pos = 0;
+
+    // Read from the text file
+    getline(myFile, start);
+    if (myFile.is_open()) {
+        // read lines
+        while (!myFile.eof()) {
+            getline(myFile, input);
+
+            // Split the line by delimiter
+            while ((pos = input.find(delimiter)) != string::npos) {
+                token = input.substr(0, pos);
+                input.erase(0, pos + delimiter.length());
+            }
+
+            pairs.push_back({ token, input });
+            //cout << token << endl;
+            //cout << input << endl;
+            loop++;
+        }
+        // Close the file
+        myFile.close();
+    }
+}
+
